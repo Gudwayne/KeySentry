edd0107f89528d92915c7071a35a2e9c107dc464
Godwin Chujor <85105115+Gudwayne@users.noreply.github.com>
2023-10-17 10:17:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+//display modal on click
+
+const modalWrapper = document.querySelector(".modals-wrapper");
+const sentry = document.querySelector(".thekeys");
+
+function displayModal(id){
+
+    if (id === 'keys-modal'){
+        modalWrapper.style.display = "none";
+        keysModal(id);
+    }else{
+        sentry.style.display = "none";
+        modalCall(id);
+    }
+
+}
+
+function keysModal(id){
+    const modal = document.getElementById(id);
+    modal.style.display = "flex";
+
+    //close modal
+    const hide = document.getElementById("close-keys");
+    hide.addEventListener("click", () =>{
+        modal.style.display = "none"
+        // document.querySelector("header").style.display = "unset";
+    })
+}
+
+function modalCall(id){
+
+    const addmode = document.getElementById("add-password-modal");
+    const genmode = document.getElementById("generator-modal");
+
+    const modal = document.getElementById(id);
+    modalWrapper.style.display = "flex";
+    if (id === 'add-password-modal'){
+        genmode.style.display = "none";
+        modal.style.display = "flex";
+    }else if(id === 'generator-modal'){
+        addmode.style.display = "none";
+        modal.style.display = "flex";
+    }
+
+    //close modal
+    const close = document.getElementById("close-modal");
+    close.addEventListener("click", () =>{
+        modalWrapper.style.display = "none";
+        modal.style.display = "none"
+        // document.querySelector("header").style.display = "unset";
+    })
+}
+
+// copy to clipboard
+const copies = document.querySelectorAll(".copy");
+copies.forEach(copy =>{
+    copy.onclick = () =>{
+        let elementToCopy = copy.previousElementSibling;
+        elementToCopy.select();
+        document.execCommand("copy"); 
+    }
+})
+
+
+const actions = document.querySelectorAll(".actions");
+if (actions){
+    actions.forEach(action =>{
+        action.onclick = () =>{
+            const links = action.querySelectorAll("a");
+            links.forEach(link =>{
+                link.style.display = "flex";
+            })
+            setTimeout(function(){
+                links.forEach(link =>{
+                    link.style.display = "none";
+                })}
+            , 3000)
+        }
+    })
+}
+
+const heading = document.getElementById('typewriter-heading');
+const text = "Key Sentry Manager";
+let index = 0;
+
+function typeText() {
+    if (index < text.length) {
+        heading.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeText, 120); // Adjust the typing speed (in milliseconds) here
+    } else {
+        // Text has been fully typed
+        setTimeout(() => {
+            // Clear the text and reset the index
+            heading.innerHTML = '';
+            index = 0;
+            typeText(); // Start typing again
+        }, 6000); // Adjust the pause before the loop (in milliseconds) here
+    }
+}
+
+typeText();
