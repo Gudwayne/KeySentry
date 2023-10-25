0abeec1066205ee998b4ff8ed669c1d8737e5b15
Godwin Chujor <85105115+Gudwayne@users.noreply.github.com>
2023-10-25 13:34:33 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -100,3 +100,15 @@ function typeText() {
 }
 
 typeText();
+
+
+function generateStrongPassword(length) {
+    const key = document.getElementById("keygen");
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    key.value = password;
+}
